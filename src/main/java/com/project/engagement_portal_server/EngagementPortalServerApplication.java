2726aa10903ba742633e3a370306014854257949
Suetam94 <mateusviniciusdasilva@outlook.com>
2025-01-16 17:07:47 -0300

    chore: basic spring boot app config

---
+++ src/main/java/com/project/engagement_portal_server/EngagementPortalServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.engagement_portal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngagementPortalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngagementPortalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/engagement_portal_server/EngagementPortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.engagement_portal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementPortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
